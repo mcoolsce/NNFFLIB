38bf4622dbb154475417c55adfc0c4fc7bba6572
Maarten Cools-Ceuppens <maarten.coolsceuppens@ugent.be>
2021-05-18 12:00:22 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nnff',
+    version='0.0.1',
+    description='A Neural Network Force Field package',
+    author='Maarten Cools-Ceuppens',
+    packages=['nnff'],
+    include_package_data=True
+    )
+
